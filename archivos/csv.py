971f899b8b2d46b19a53a674ad66bb6e4fe6869e
Nahuel <nahuel.profesor@gmail.com>
2023-09-15 23:58:39 -0300

    módulo csv de ejemplo agregado

---
+++ archivos/csv.py
@@ -0,0 +1,22 @@
+import csv
+
+# escribir
+with open("archivos/archivo.csv", "w") as archivo:
+    writer = csv.writer(archivo)
+    writer.writerow(["twit_id", "user_id", "text"]) 
+    writer.writerow([1000, 1, "Este es un twit"]) 
+    writer.writerow([1001, 2, "otro twit!"])
+    
+
+# leer 
+with open("archivos/archivo.csv") as archivo:
+    reader = csv.reader(archivo)
+    print(list(reader))
+    archivo.seek(0)
+    for linea in reader:
+        print(linea)
+        
+# actualizar CSV
+with open("archivos/archivo.csv") as r, open("archivos/archivo_temp.csv", "w") as w:
+    reader = csv.reader(r)
+    writer = csv.writer
