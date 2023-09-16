ee1572472610ebf69b85d3e4a289ce7127287346
Nahuel <nahuel.profesor@gmail.com>
2023-09-16 00:04:58 -0300

    ejemplos con módulo csv agregados

--- archivos/csv.py
+++ archivos/csv.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 # escribir
 with open("archivos/archivo.csv", "w") as archivo:
@@ -19,4 +20,11 @@ with open("archivos/archivo.csv") as archivo:
 # actualizar CSV
 with open("archivos/archivo.csv") as r, open("archivos/archivo_temp.csv", "w") as w:
     reader = csv.reader(r)
-    writer = csv.writer
+    writer = csv.writer(w)
+    for linea in reader:
+        if linea[0] == "1000":
+            writer.writerow([1000, 1, "texto modificado"])
+        else:
+            writer.writerow(linea)
+    os.remove("archivos/archivo.csv")
+    os.rename("archivos/archivo_temp.csv", "archivos/archivo.csv")
