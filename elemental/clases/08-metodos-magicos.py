2066bc2ac40e89b1ca318a00dc9da5978668b9fb
Nahuel <nahuel.profesor@gmail.com>
2023-09-27 21:39:07 -0300

    explicación sobre el destructor agregada

--- elemental/clases/08-metodos-magicos.py
+++ elemental/clases/08-metodos-magicos.py
@@ -9,6 +9,9 @@ class Perro:
         self.nombre = nombre
         self.edad = edad
         
+    def __del__(self):
+        print(f"Chao perro {self.nombre}")    
+        
     def __str__(self):
         return f"Clase Perro: {self.nombre}"
         
@@ -21,6 +24,9 @@ print(perro)
 texto = str(perro)
 print(texto)
 
+del perro
 
+# el constructor es un método mágico que se ejecuta cuando creamos un objeto
+# el destructor es un método mágico que se ejecuta cuando eliminamos un objeto
 
 
