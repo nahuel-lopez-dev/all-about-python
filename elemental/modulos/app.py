0fb2f777b5545a70d295bbd9f1fe1a21a3184051
Nahuel <nahuel.profesor@gmail.com>
2023-10-02 02:00:29 -0300

    ejemplo de trabajo con módulos agregado

---
+++ elemental/modulos/app.py
@@ -0,0 +1,9 @@
+from usuario_impuestos import guardar, pagar_impuestos
+
+guardar()
+pagar_impuestos()
+
+# no importar con *
+# convención de nombre con múltiples palabras
+# palabra1_palabra2.py
+
---
+++ elemental/modulos/usuario_impuestos.py
@@ -0,0 +1,8 @@
+def guardar():
+    print("guardando")
+
+    
+def pagar_impuestos():
+    print("pagando impuestos")
+    
+    
