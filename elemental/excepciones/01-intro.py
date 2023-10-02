7fa57bdba3a4abf4439240d0d73dd23399ce2df5
Nahuel <nahuel.profesor@gmail.com>
2023-10-02 01:49:37 -0300

    ejemplos con excepciones agregado

---
+++ elemental/excepciones/01-intro.py
@@ -0,0 +1,11 @@
+# print("")
+# lista = [1, 2]
+# lista[45] # IndexError: list index out of range
+
+try:
+    n1 = int(input("Ingresa primer número:"))
+except:
+    print("ocurrió un error :(")
+    
+
+
